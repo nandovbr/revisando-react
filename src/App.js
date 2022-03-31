1b265f8576081f2eb47f66b6f7c8be1c541c6528
nandovbr <nandovbr15@hotmail.com>
2022-03-31 17:17:06 -0300

    iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Estudando React</h1>
+      <p>Olá react!</p>
+    </div>
+  );
+}
+
+export default App;
